697529c9a47bdc55b567bfdca50473532b234b72
Brilloku <bryankuria117@gmail.com>
2025-11-11 22:14:34 +0300

    1

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders bug tracker title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/bug tracker/i);
-  expect(titleElement).toBeInTheDocument();
-});
